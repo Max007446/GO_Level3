e63147b07190bfcf453e1da27788cae956754a96
MaxPsk <max00744@gmail.com>
2021-05-03 20:41:21 +0300

    pre 3

--- dubli/dubli_test.go
+++ dubli/dubli_test.go
@@ -10,5 +10,5 @@ func testwalk_single(t *testing.T, path *string) { // is unused (deadcode)
 	assert.DirExists(t, *path, "norm")
 }
 func testwalk_multi(t *testing.T, path *string) { // is unused (deadcode)
-	assert.FileExists(t, *path, "norm")
+	assert.FileExists(t, *path, "norm2")
 }
